c0b96e4ec1394dc2a8e8c3170010d2693db4d7db
Jana Valášková <jancav@seznam.cz>
2021-06-06 17:47:18 +0200

    cyklus--final

--- main.js
+++ main.js
@@ -2,18 +2,14 @@
 // Konzoli otevřeš v Chrome stiskem F12 (na Windows) nebo Command-Option-I (na Macu)
 
 
-let cisla = [];
-
+/*let cisla = [];
 // První příklad - vypiš vzestupně čísla od 0 do 10
-
 let i = 0;
 while (i<=9) {
     i++;
     cisla.push(i);
     vypisCislo(i, "#prvni");
     console.log(i);
-    
-   
 }
 // Druhý příklad - zkopíruj předchozí příklad
 // a vyzkoušej, jaký je rozdíl mezi <= a <
@@ -23,27 +19,46 @@ while(z<9) {
     z++;
     cisla.push(z);
     vypisCislo(z, "#druhy");
-    
 }
 
 // Třetí příklad - vypiš sestupně čísla od 10 do 0
-
 let y=11; 
 while (y>=2) {
     y--;
     cisla.push(y);
     vypisCislo(y, "#treti");
-    
 } 
-
-
 function vypisCislo(cislo, kamHoNapsat) {
     document.querySelector(kamHoNapsat).innerHTML += cislo + "\n";
+}*/
+
+//_____________________________________________________________________-
+  //MŮJ PODÚKOL Z NETU    
+
+//počítá aktuální věk
+function calculateAge(birth) {   
+    return 2021 - birth;
+}
+    
+//počítá odchod do důchodu
+function yearsUntilRetirement(year, firstName) {
+    let age = calculateAge(year);
+    let retirement = 65 - age;
+    if (retirement >=0) {
+        vypisVek(retirement, firstName, "#ctvrty");
+        //console.log("Počet let do důchodu " + retirement + " let" + " u " + firstName);
+    } else {
+        retirement = -(retirement);
+        vypisVek(retirement, firstName, "#ctvrty");
+        //console.log(firstName + " je už v důchodu " + retirement + " let.");
+    } 
+}
+    
+function vypisVek(retirement, firstName, semSeToVypise) {
+    document.querySelector(semSeToVypise).innerHTML = "Počet let do důchodu " + retirement + " let" + " u " + firstName;
 }
 
+yearsUntilRetirement(1960, "John"); 
 
-//doprac.výpis na 3 řádky, + to pushnout přes commit 
- //+založit větev develop přes branch (viz konec videa)
-  //vyprac.UKOL WHILE CYKLU
 
 
